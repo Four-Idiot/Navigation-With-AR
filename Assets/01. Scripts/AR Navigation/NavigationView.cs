de5a560216d158665049782e123659b775b7913a
ggzz303@gmail.com <147801927+YejiReginaKim@users.noreply.github.com>
2024-02-15 12:16:34 +0900

    마크업

--- Assets/01. Scripts/AR Navigation/NavigationView.cs
+++ Assets/01. Scripts/AR Navigation/NavigationView.cs
@@ -1,3 +1,8 @@
 public class NavigationView : UIView
 {
+    private int currentDestination;
+    public void Init(int destinationCoords)
+    {
+        currentDestination = destinationCoords;
+    }
 }
