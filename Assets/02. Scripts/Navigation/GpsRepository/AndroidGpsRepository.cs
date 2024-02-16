28298c3e6df8ab1d0a4c16ee26c7e27bbd1566c3
Jung wan <wjddhksaos@gmail.com>
2024-02-16 16:46:48 +0900

    디렉토리 이름 변경, gps repository 추가(구현은 아직)

---
+++ Assets/02. Scripts/Navigation/GpsRepository/AndroidGpsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class AndroidGpsRepository : IGpsRepository
+{
+    private LocationService locationService;
+    private Coords coords;
+
+    public void StartLocationService(Action callback)
+    {
+        if (Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+            locationService.Start();
+        else
+            callback?.Invoke();
+    }
+
+    public async Task<Coords> FindCoordsByCurrentLocation()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+        }
+        else
+        {
+        }
+        throw new NotImplementedException();
+    }
+}
