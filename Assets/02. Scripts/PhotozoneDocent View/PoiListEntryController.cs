976f954b88ad45f78088906cfa48dd2714580745
Jung wan <wjddhksaos@gmail.com>
2024-02-26 16:48:38 +0900

    feat: 포토존/도슨트 상세페이지

--- Assets/02. Scripts/PhotozoneDocent View/PoiListEntryController.cs
+++ Assets/02. Scripts/PhotozoneDocent View/PoiListEntryController.cs
@@ -2,6 +2,7 @@
 
 public class PoiListEntryController
 {
+    private VisualElement container;
     private Label nameLabel;
     private Label descriptionLabel;
 
@@ -9,11 +10,19 @@ public class PoiListEntryController
     {
         nameLabel = visualElement.Q<Label>("name");
         descriptionLabel = visualElement.Q<Label>("description");
+        container = visualElement.Q<VisualElement>("photozone-list-item");
     }
 
     public void SetPoiData(Marker poi)
     {
         nameLabel.text = poi.Name;
         descriptionLabel.text = poi.Address;
+        container.RegisterCallback<ClickEvent>(OnItemClicked);
+    }
+
+    private void OnItemClicked(ClickEvent evt)
+    {
+        var detailView = UINavigation.Instance.Push(UIViewIndex.PHOTOZONE_DOCENT_DETAIL) as PhotoZoneDocentDetailView;
+        detailView.Init(nameLabel.text, descriptionLabel.text);
     }
 }
